e4831aa737aa9c810a96049af3893709586b13f3
Terrillo Walls <terrillo@terrillo.me>
2015-01-15 13:57:01 -0600

    The BASIC idea

---
+++ alpha.js
@@ -0,0 +1,48 @@
+var alpha = {
+	
+	
+	//(1/15/15)	
+	//===================================================================
+	//======= Sending and Getting Request
+	//===================================================================
+	post: function(url, array) {
+		var xmlhttp;
+		if (window.XMLHttpRequest) {
+			xmlhttp=new XMLHttpRequest();
+		}
+		else {
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.open("POST",url,true);
+		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		xmlhttp.send(array);
+		// alpha.post('http://google.com', 'name=john&email=john@gmail.com'); 
+	},
+	
+	
+	//(1/15/15)	
+	//===================================================================
+	//======= The URL
+	//===================================================================
+	thisurl: function() {
+		return window.location.href;
+		// alpha.thisurl(); 
+	}, 
+	
+	
+	//(1/15/15)	
+	//===================================================================
+	//======= Inputs 
+	//===================================================================
+	value: function(name) {
+		if(name.indexOf("#") >= 0) {
+			// By ID
+			return document.getElementById(name).value;
+		}
+		else {
+			// By Class Name
+			return document.getElementsByClassName(name).value;
+		}
+		// alpha.value('name'); 
+	}
+}
